a953f9144e5ed974dd8e75aab64cb448b03dfb5f
Rana.ismael <rana4ismael@gmail.com>
2022-02-20 16:31:03 -0500

    second

--- src/main/java/DAO/BankJdbcDaoImpl.java
+++ src/main/java/DAO/BankJdbcDaoImpl.java
@@ -20,7 +20,7 @@ public class BankJdbcDaoImpl implements BankDAO {
 	public static final Logger LOG = LogManager.getLogger(BankJdbcDaoImpl.class);
 
 	@Override
-	public List<Customer> fetchAllCustomers() throws SystemException, AccountNotFoundException{
+	public List<Customer> fetchAllCustomers() throws SystemException, AccountNotFoundException {
 		LOG.info("Exit fetchAllCustomers() in DAO");
 		List<Customer> allCustomer = new ArrayList<Customer>();
 		Connection conn = DBUtil.obtainConnection();
@@ -29,12 +29,12 @@ public class BankJdbcDaoImpl implements BankDAO {
 			String query = "select * from customer_details";
 
 			ResultSet rs = stmt.executeQuery(query);
-		
+
 // iterate through the result set 
 			while (rs.next()) {
 
-				Customer customer = new Customer(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getDouble(4));
-				
+				Customer customer = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4));
+
 				allCustomer.add(customer);
 			}
 		} catch (SQLException e) {
@@ -85,11 +85,11 @@ public class BankJdbcDaoImpl implements BankDAO {
 			String query = "SELECT * FROM customer_details WHEERE account_no=" + accountNo;
 			ResultSet rs = stmt.executeQuery(query);
 			if (rs.next()) {
-				customer = new Customer(rs.getInt(1),rs.getString(2), rs.getString(3), rs.getDouble(4));
+				customer = new Customer(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4));
 
 			}
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 		LOG.info("Exit fetchAcount() in DAO");
@@ -101,7 +101,7 @@ public class BankJdbcDaoImpl implements BankDAO {
 		LOG.info("Entered createAccount() in DAO");
 		Connection conn = DBUtil.obtainConnection();
 		try {
-			Statement stmt =  conn.createStatement();
+			Statement stmt = conn.createStatement();
 			int lastAccount = 0;
 			String query1 = "SELECT MAX(account_no) FROM customer_details";
 			ResultSet rs = stmt.executeQuery(query1);
@@ -112,7 +112,7 @@ public class BankJdbcDaoImpl implements BankDAO {
 
 			String query2 = "INSERT INTO customer_details VALUES(" + newAccount + ",'" + cus.getName() + "','"
 					+ cus.getPassword() + "','" + cus.getBalance() + "')";
-			int rows =  stmt.executeUpdate(query2);
+			int rows = stmt.executeUpdate(query2);
 			cus.setAccountNo(newAccount);
 		} catch (SQLException e) {
 			throw new SystemException();
@@ -121,7 +121,6 @@ public class BankJdbcDaoImpl implements BankDAO {
 		return cus;
 	}
 
-
 	@Override
 	public boolean transfer(int fromAccountNo, int toAccountNo, double amount) throws SystemException, Exception {
 		Connection conn = DBUtil.obtainConnection();
@@ -129,33 +128,26 @@ public class BankJdbcDaoImpl implements BankDAO {
 		try {
 			stmt = (Statement) conn.createStatement();
 
-			
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
-		String query1 = "UPDATE account_no SET balance=balance-"+amount+" WHERE account="+fromAccountNo;
-		String query2 = "UPDATE account_no SET balance=balance+"+amount+" WHERE account_no="+toAccountNo;
+		String query1 = "UPDATE customer_details SET balance=balance-" + amount + " WHERE account_no=" + fromAccountNo;
+		String query2 = "UPDATE customer_details SET balance=balance+" + amount + " WHERE account_no=" + toAccountNo;
 
-		// some logic to check if balance of fromAccount is > transferMoney
 		
-		conn.setAutoCommit(false); // marks the start of the transaction
+
+		 
 		int rows1 = stmt.executeUpdate(query1);
-		// if something happened here
-		if(true)
-			throw new SQLException();
-		int rows2 = stmt.executeUpdate(query2);
-		conn.commit();// marks the end of the transaction and the changes are commited to the DB
-							//transaction was successful and data is in the new state of consistency
+        int rows = stmt.executeUpdate(query2);
+		
 		
 		System.out.println("Money transfered...");
-	
-		
-			conn.rollback();// marks the end of the transaction but the transaction has failed, data
-									// is in old state of consistency
-			System.out.println("Transaction failed...");
-		
-return true;
 
-}
+		
+		
+
+		return true;
+
+	}
 }
